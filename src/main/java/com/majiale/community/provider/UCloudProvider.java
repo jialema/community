fbe3cdaea5b5354da847f8dd1b71806421dc9a15
jialema <jialema1996@qq.com>
2020-10-08 01:10:13 +0100

    55 上传图片 - 使用SDK生成预览地址

--- src/main/java/com/majiale/community/provider/UCloudProvider.java
+++ src/main/java/com/majiale/community/provider/UCloudProvider.java
@@ -7,6 +7,8 @@ import cn.ucloud.ufile.auth.UfileObjectLocalAuthorization;
 import cn.ucloud.ufile.bean.PutObjectResultBean;
 import cn.ucloud.ufile.exception.UfileClientException;
 import cn.ucloud.ufile.exception.UfileServerException;
+import com.majiale.community.exception.CustomizeErrorCode;
+import com.majiale.community.exception.CustomizeException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,8 @@ public class UCloudProvider {
     @Value("${ucloud.ufile.private-key}")
     private String privateKey;
 
+    private String bucketName = "majiale";
+
     public String upload(InputStream fileStream, String mimeType, String fileName) {
 
         // 获取文件名
@@ -29,7 +33,7 @@ public class UCloudProvider {
         if (filePaths.length > 1) {
             generatedFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length - 1];
         } else {
-            return null;
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
         }
 
         try {
@@ -38,33 +42,29 @@ public class UCloudProvider {
             // 配置地区和域名
             ObjectConfig config = new ObjectConfig("cn-bj", "ufileos.com");
             PutObjectResultBean response = UfileClient.object(objectAuthorization, config)
-                    .putObject(fileStream, fileStream.available(), mimeType)
+                    .putObject(fileStream, fileStream.available(), mimeType) // 这里于视频教程略有不同，因为api变了
                     .nameAs(generatedFileName)
-                    .toBucket("majiale")
-                    /**
-                     * 是否上传校验MD5, Default = true
-                     */
-                    //  .withVerifyMd5(false)
-                    /**
-                     * 指定progress callback的间隔, Default = 每秒回调
-                     */
-                    //  .withProgressConfig(ProgressConfig.callbackWithPercent(10))
-                    /**
-                     * 配置进度监听
-                     */
+                    .toBucket(bucketName)
                     .setOnProgressListener((bytesWritten, contentLength) -> {
 
                     })
                     .execute();
+            if (response != null && response.getRetCode() == 0) {
+                String url = UfileClient.object(objectAuthorization, config)
+                        .getDownloadUrlFromPrivateBucket(generatedFileName, bucketName, 24 * 60 * 60)
+                        .createUrl();
+                return url;
+            } else {
+                throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+            }
         } catch (UfileClientException e) {
             e.printStackTrace();
-            return null;
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
         } catch (UfileServerException e) {
             e.printStackTrace();
             return null;
         } catch (Exception e) {
-            return null;
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
         }
-        return generatedFileName;
     }
 }
